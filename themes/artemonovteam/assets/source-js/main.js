2b2715da3accb72ef77a0fa5f7660b0d85d7635c
Slavik <kulbij.intertech@gmail.com>
2017-08-03 18:05:06 +0300

    update

--- themes/artemonovteam/assets/source-js/main.js
+++ themes/artemonovteam/assets/source-js/main.js
@@ -1,33 +1,63 @@
-$('.button-nav').on('click', function () {
-    $('.humburger').toggleClass('show-hide');
+$(document).ready( function () {
+    //validate init
+    $("form").validate();
 
-});
 
-$(document).ready(function () {
+    $("input.phone-mask").mask("+38(999)999-9999");
+
+    $('.button-nav').on('click', function () {
+        $('.humburger').toggleClass('show-hide');
+
+    });
 
     $(".button-in-item").on('click', function () {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
         $('.modal-in-item').css('display', 'flex').animate({opacity: 1}, 500);
     });
 
     $(".button-in").on('click', function () {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
         $('.modal-in').css('display', 'flex').animate({opacity: 1}, 500);
     });
 
-    $(".button-reg").on('click', function () {
+    $("body").on("click", '.button-reg', function() {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
         $('.modal-reg').css('display', 'flex').animate({opacity: 1}, 500);
     });
 
 
     $(".button-cat").on('click', function () {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
         $('.modal-cat').css('display', 'flex').animate({opacity: 1}, 500);
     });
 
+    $("body").on("click", '.button-prog', function(){
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
+        $('.modal-prog').css('display', 'flex').animate({opacity: 1}, 500);
+    });
 
+    $("body").on("click", '.button-callback', function() {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
+        $('.modal-callback').css('display', 'flex').animate({opacity: 1}, 500);
+    });
+    $(".button-change-contact").on('click', function () {
+        $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'hidden');
+        $('.modal-change-contact').css('display', 'flex').animate({opacity: 1}, 500);
+    });
 
 
     $('body').on('click', '.close', function () {
         $('.modal').css('display', 'none');
+        $('.wrapper').css('overflow', 'auto');
     });
 
 });
+
 
